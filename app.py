60df807695949622f0cfa70f292a9d65c6f3b289
nick919191 <nicola.silvestre091@gmail.com>
2024-10-24 11:29:44 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Dashboard per i tool")
+st.write("Benvenuto alla dashboard!")
+
+# Aggiungi pulsanti o link per i tool
+st.button("Clicca qui per Tool 1")
+st.button("Clicca qui per Tool 2")
